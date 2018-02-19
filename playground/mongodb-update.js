eabd12be0a012d4fe50431aee40dc7ecd6acfb25
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 16:21:02 +0100

    challenge() Update and inc a user
    Update name property and increment age for a selected user
    
    S6,L68

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,19 +7,47 @@ const consoleErrorMongo = (error, db) => console.log(concatError(error));
 // findOneAndUpdate
 
 let clientDB = {};
+
+// MongoClient.connect('mongodb://localhost:27017/TodoApp')
+//   .then((client) => {
+//     clientDB = client;
+//     return client.db('TodoApp');
+//   })
+//   .then(db => {
+//     return db.collection('Todos').findOneAndUpdate(
+//       {
+//         _id: new ObjectID('5a8ae3e5ea2723e8a2009ea3')
+//       },
+//       {
+//         $set: {
+//           completed: true
+//         }
+//       },
+//       {
+//         returnOriginal: false
+//       });
+//   })
+//   .then(console.log)
+//   .catch(consoleErrorMongo)
+//   .then(() => clientDB.close());
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp')
   .then((client) => {
     clientDB = client;
     return client.db('TodoApp');
   })
   .then(db => {
-    return db.collection('Todos').findOneAndUpdate(
+    return db.collection('Users').findOneAndUpdate(
       {
-        _id: new ObjectID('5a8ae3e5ea2723e8a2009ea3')
+        _id: new ObjectID('5a8acacaadf6fd0a92167746')
       },
       {
         $set: {
-          completed: true
+          name: 'Alberto Eyo'
+        },
+        $inc: {
+          age: 1
         }
       },
       {
@@ -29,4 +57,3 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp')
   .then(console.log)
   .catch(consoleErrorMongo)
   .then(() => clientDB.close());
-
