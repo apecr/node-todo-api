e6c7031d9f50a7bb68931bff06324e2a93b1056f
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 16:01:45 +0100

    challenge(mongodb-delete.js)
    Deleting the Alberto Eyo users and the id = 123 user
    
    S6,L67

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,6 +8,21 @@ const consoleErrorMongo = (error, db) => console.log(concatError(error));
 // findOneAndDelete
 
 
+// let clientDB = {};
+// MongoClient.connect('mongodb://localhost:27017/TodoApp')
+//   .then((client) => {
+//     clientDB = client;
+//     return client.db('TodoApp');
+//   })
+//   .then(db => {
+//     db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//       console.log('Todos');
+//       console.log(result);
+//     }, err => console.log('Unable to delete todos', err));
+//   })
+//   .catch(consoleErrorMongo)
+//   .then(() => clientDB.close());
+
 let clientDB = {};
 MongoClient.connect('mongodb://localhost:27017/TodoApp')
   .then((client) => {
@@ -15,10 +30,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp')
     return client.db('TodoApp');
   })
   .then(db => {
-    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-      console.log('Todos');
-      console.log(result);
-    }, err => console.log('Unable to delete todos', err));
+    return db.collection('Users').deleteMany({name: 'Alberto Eyo'})
+      .then((result) => {
+        console.log('Users');
+        console.log(result);
+        return result;
+      }, err => console.log('Unable to delete todos', err))
+      .then(() => {
+        return db.collection('Users').findOneAndDelete({_id: 123}).then(result => console.log('Deleted', result));
+      });
   })
   .catch(consoleErrorMongo)
   .then(() => clientDB.close());
+
