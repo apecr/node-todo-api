60e18f167f3d8b0ca2d30292fca4ee16517fc22e
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 16:21:56 +0100

    refactor() Clean code
    Clean comments and blank lines
    
    S6,L68

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -3,35 +3,7 @@ const {MongoClient, ObjectID} = require('mongodb');
 const concatError = (error) => `Unable to connect to MongoDB server: ${error}`;
 const consoleErrorMongo = (error, db) => console.log(concatError(error));
 
-
-// findOneAndUpdate
-
 let clientDB = {};
-
-// MongoClient.connect('mongodb://localhost:27017/TodoApp')
-//   .then((client) => {
-//     clientDB = client;
-//     return client.db('TodoApp');
-//   })
-//   .then(db => {
-//     return db.collection('Todos').findOneAndUpdate(
-//       {
-//         _id: new ObjectID('5a8ae3e5ea2723e8a2009ea3')
-//       },
-//       {
-//         $set: {
-//           completed: true
-//         }
-//       },
-//       {
-//         returnOriginal: false
-//       });
-//   })
-//   .then(console.log)
-//   .catch(consoleErrorMongo)
-//   .then(() => clientDB.close());
-
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp')
   .then((client) => {
     clientDB = client;
