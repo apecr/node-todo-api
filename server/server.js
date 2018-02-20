34b4bac39f701d25310de388009885a85b74f215
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-20 08:35:51 +0100

    challenge() Include the user schema
    Simple schema for user with only email property
    
    S6,L71

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-const logError = e => console.log('Unable to save todo', e);
+const logTodoError = e => console.log('Unable to save todo', e);
 const Todo = mongoose.model('Todo', {
   text: {
     type: String,
@@ -25,4 +25,28 @@ const newTodo = new Todo({text: 'Edit this video', completed: false});
 
 newTodo.save().then((doc) => {
   console.log('Saved todo', doc);
-}, logError);
+}, logTodoError);
+
+// User model
+// email property - require it - trim it - set string, min length of 1
+// Create a new one (see that one fails)
+
+const User = mongoose.model('Users',
+  {
+    email: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true
+    }
+  });
+
+const newUserOK = new User({email: 'as@as.as'});
+const newUserKO = new User({email: '    '});
+
+newUserOK
+  .save()
+  .then((doc) => console.log('Saved todo', doc))
+  .then(() => newUserKO.save())
+  .then((doc) => console.error('This doc was not supossed to be saved', doc))
+  .catch(error => console.log('User without email not saved, OK:', error));
