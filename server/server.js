cfd10beaafb01f89e5a56a19748f60f6fbc9a48e
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-20 17:31:05 +0100

    chore() Set for heroku
    Set heroku
    
    S6,L80

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
     .catch(() => res.status(400).send());
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening at port ${port}`));
 
 module.exports = { app };
 
