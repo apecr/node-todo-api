f3d78b751242b6e4b743dc498e9e0c18680a2e22
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-23 09:40:29 +0100

    chore() Include bcryptjs
    Include bcryptjs for hashing passwords, and examples in playground
    
    S9,L92

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256: sha256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (error, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$zL8rEfH4xfaC9U8g8o3/Jujdc8QWi/8WI74EhYJABup9uXo3PyX56';
+bcrypt.compare(password, hashedPassword, (error, result) => {
+  console.log(result);
+});
+
+// const data = {
+//   id: 10
+// };
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // const message = 'I am user number 3';
 // const hash = sha256(message).toString();
