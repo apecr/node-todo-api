8fe30db8153f3edb93c26ff4a45844e1cf9a2d70
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-27 14:01:05 +0100

    refactor(server/config/config.js) Get json in certain environments
    Only get the config.json in test and development environments
    
    S8,L102

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
-const config = require('./config.json');
-
 const prepareEnvironment = (() => {
   const env = process.env.NODE_ENV || 'development';
   if (env === 'development' || env === 'test') {
+    const config = require('./config.json');
     Object.keys(config[env]).forEach(key => process.env[key] = config[env][key]);
   }
   return env;
